7a7702fcec5d93cc35de565db7b35ca5c8067824
Luke Cottage <johntitorwastaken@gmail.com>
2020-11-05 20:35:59 +0100

    [POC]: debug - testing envs

--- .github/scripts/maven_information.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# export variables from pom.xml
-
-echo "POM_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)"
-echo "POM_GROUPID=$(mvn help:evaluate -Dexpression=project.groupId -q -DforceStdout)"
-echo "POM_ARTIFACTID=$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout)"
-echo "POM_NAME=$(mvn help:evaluate -Dexpression=project.name -q -DforceStdout)"
-echo "POM_DESCRIPTION=$(mvn help:evaluate -Dexpression=project.description -q -DforceStdout)"
